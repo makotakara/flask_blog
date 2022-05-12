e69fec4d7a367258bd662a9a56b2df18a0c8fe8c
Bogdanov Roman <romanbog@gmail.com>
2022-05-12 16:47:41 +0300

    add database, models.py, post_detail.html. show all posts on page blog

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from config import Configuration 
-from posts.blueprint import posts
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
-app.register_blueprint(posts, url_prefix='/blog')
+
+db = SQLAlchemy(app)
